7c2cb824cc8acc524771d87ec2add03e5b31b5c8
haymannko <hay@gmail.com>
2025-12-03 01:18:58 -0800

    updated about s3

--- s3.tf
+++ s3.tf
@@ -83,35 +83,3 @@ resource "aws_s3_bucket_website_configuration" "example" {
   }
 }
 
-resource "aws_s3_bucket_policy" "public_read" {
-  bucket = aws_s3_bucket.frontend-web-bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.frontend-web-bucket.arn}/*"
-      }
-    ]
-  })
-
-  depends_on = [
-    aws_s3_bucket_public_access_block.example,
-    aws_s3_bucket_acl.example,
-    aws_s3_bucket_ownership_controls.example
-  ]
-}
-
-
-
-output "bucket_website_endpoint" {
-  value = aws_s3_bucket_website_configuration.example.website_endpoint
-}
-
-output "bucket_website_domain" {
-  value = aws_s3_bucket_website_configuration.example.website_domain
-}
