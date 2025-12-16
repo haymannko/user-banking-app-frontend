1dec8d15208b0d12feec867fe4e58d6e7a51ce27
haymannko <hay@gmail.com>
2025-12-16 08:48:44 -0800

    modified cloudfront.tf

--- infra/cloudfront.tf
+++ infra/cloudfront.tf
@@ -8,7 +8,7 @@ locals {
 
 
 resource "aws_cloudfront_origin_access_control" "default" {
-  name                              = "default-oac-name-111"
+  name                              = "default-oac-name-1111"
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
